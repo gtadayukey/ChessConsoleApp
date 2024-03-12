183f4967a27f93a32b21194c6504578161c8c476
gtadayukey <gtadayukey@gmail.com>
2024-03-12 08:14:51 -0300

    Inicializing project repository

---
+++ ChessConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ChessConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
